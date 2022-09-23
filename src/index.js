99fbc6d6cde3d4d8bc30d11e2fd35a2c2cf5bf07
Manuel <comanuel7@gmail.com>
2022-09-23 15:21:53 -0500

    Mi primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {saludar} from './js/componentes.js'
+import './styles.css'
+
+
+const nombre = 'Manuel'
+console.log(nombre)
+
+saludar('Manuel')
---
+++ src/js/componentes.js
@@ -0,0 +1,20 @@
+
+import '../css/components.css';
+
+import webpacklogo from '../assets/img/webpack-logo.png'
+
+export const saludar = (nombre)=>{
+    console.log('crenado etiqueta h1 ')
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola , ${nombre}`
+    document.body.append(h1);
+    
+    
+    console.log(webpacklogo)
+    const img  = document.createElement('img');
+    img.src = webpacklogo;
+    document.body.append(img)
+
+
+}
+
